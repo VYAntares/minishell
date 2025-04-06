afa5b1b34cfce47c04e7e2b07f9b06688fb6de70
eahmeti <eahmeti@student.42lausanne.ch>
2025-04-06 16:44:12 +0200

    plus que les redirection

--- obj/src/builtins/builtins.d
+++
@@ -1,8 +0,0 @@
-obj/src/builtins/builtins.o: src/builtins/builtins.c minishell.h \
-  /opt/homebrew/opt/readline/include/readline/readline.h \
-  /opt/homebrew/opt/readline/include/readline/rlstdc.h \
-  /opt/homebrew/opt/readline/include/readline/rltypedefs.h \
-  /opt/homebrew/opt/readline/include/readline/keymaps.h \
-  /opt/homebrew/opt/readline/include/readline/chardefs.h \
-  /opt/homebrew/opt/readline/include/readline/tilde.h \
-  /opt/homebrew/opt/readline/include/readline/history.h libft/libft.h
