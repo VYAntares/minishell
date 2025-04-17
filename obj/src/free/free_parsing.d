78b6131585ee8c9838b45565b063d6900b30f043
eahmeti <eahmeti@student.42lausanne.ch>
2025-04-17 15:07:54 +0200

    norminette execute_command

--- obj/src/free/free_parsing.d
+++
@@ -1,9 +0,0 @@
-obj/src/free/free_parsing.o: src/free/free_parsing.c \
-  src/free/../../minishell.h \
-  /opt/homebrew/opt/readline/include/readline/readline.h \
-  /opt/homebrew/opt/readline/include/readline/rlstdc.h \
-  /opt/homebrew/opt/readline/include/readline/rltypedefs.h \
-  /opt/homebrew/opt/readline/include/readline/keymaps.h \
-  /opt/homebrew/opt/readline/include/readline/chardefs.h \
-  /opt/homebrew/opt/readline/include/readline/tilde.h \
-  /opt/homebrew/opt/readline/include/readline/history.h libft/libft.h
