1e1fc2dc85149f3e2db9abf64736003c3e6392f5
eahmeti <eahmeti@student.42lausanne.ch>
2025-04-19 16:00:11 +0200

    rebuild command arg norminette

--- src/builtins/expand_env.c
+++ src/builtins/expand_env.c
@@ -6,7 +6,7 @@
 /*   By: eahmeti <eahmeti@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 11:00:00 by eahmeti           #+#    #+#             */
-/*   Updated: 2025/04/18 16:03:01 by eahmeti          ###   ########.fr       */
+/*   Updated: 2025/04/19 15:58:47 by eahmeti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -298,6 +298,127 @@ int	preprocess_dollar_quotes(t_token_word **head)
 	return (0);
 }
 
+// Vérifier si l'argument contient des guillemets
+int	should_we_split(t_token_word *list_word)
+{
+	int				has_quote;
+	int				will_split;
+	t_token_word	*word;
+	
+	has_quote = 0;
+	will_split = 0;
+	word = list_word;
+	while (word)
+	{
+		if (word->type == T_S_QUOTE || word->type == T_D_QUOTE)
+			has_quote = 1;
+		if (word->type == T_NO_QUOTE && (ft_strchr(word->content, ' ')
+				|| ft_strchr(word->content, '\t')))
+			will_split = 1;
+		word = word->next;
+	}
+	if (has_quote || !will_split)
+		return (0);
+	return (1);
+}
+
+int	count_arg_after_split(t_token_word *list_word)
+{
+	t_token_word	*word;
+	char			*tmp;
+	char			**split_words;
+	int				j;
+	
+	tmp = ft_strdup("");
+	word = list_word;
+	while (word)
+	{
+		tmp = ft_strjoin(tmp, word->content);
+		word = word->next;
+	}
+	split_words = ft_split(tmp, ' ');
+	j = 0;
+	while (split_words && split_words[j])
+		j++;
+	free(tmp);
+	if (split_words)
+		free_array(split_words);
+	return (j);
+}
+
+char	*rebuild_word(t_token_word *list_word)
+{
+	char			*tmp;
+	char			*new_tmp;
+	t_token_word	*word;
+	
+	tmp = ft_strdup("");
+	word = list_word;
+	while (word)
+	{
+		new_tmp = ft_strjoin(tmp, word->content);
+		free(tmp);
+		tmp = new_tmp;
+		word = word->next;
+	}
+	return (tmp);
+}
+
+void	fill_new_args(t_token_word *list_word, int *k, char **new_args)
+{
+	int		j;
+	char	*tmp;
+	char	**split_words;
+	
+	j = 0;
+	if (should_we_split(list_word) == 0)
+	{
+		tmp = rebuild_word(list_word);
+		new_args[(*k)++] = tmp;
+	}
+	else
+	{
+		tmp = rebuild_word(list_word);
+		split_words = ft_split(tmp, ' ');
+		j = 0;
+		while (split_words && split_words[j])
+		{
+			new_args[(*k)++] = ft_strdup(split_words[j]);
+			j++;
+		}
+		free(tmp);
+		if (split_words)
+			free_array(split_words);
+	}
+}
+
+void	replace_args_and_free(char **new_args, t_cmd *cmd, int k, int new_ac)
+{
+	int		i;
+	char	**tmp_args;
+
+	new_args[k] = NULL;
+	tmp_args = cmd->arg;
+	cmd->arg = new_args;
+	cmd->ac = new_ac;
+	if (cmd->ac > 0)
+	{
+		if (cmd->name)
+			free(cmd->name);
+		cmd->name = ft_strdup(cmd->arg[0]);
+	}
+	if (tmp_args)
+	{
+		i = 0;
+		while (tmp_args[i])
+		{
+			free(tmp_args[i]);
+			i++;
+		}
+		free(tmp_args);
+	}
+}
+
 /* 
  * Fonction pour reconstruire les arguments de la commande après expansion
  * Cette fonction remplace rebuild_command_arg et gère correctement 
@@ -306,142 +427,32 @@ int	preprocess_dollar_quotes(t_token_word **head)
 int rebuild_command_arg(t_cmd *cmd)
 {
 	int				i;
-	int				j;
 	int				k;
-	int				new_ac = 0;
+	int				new_ac;
 	char			**new_args = NULL;
-	t_token_word	*word;
-	char			*tmp;
-	char			**split_words;
-	char			**tmp_args;
-	int				has_quote;
-	int				will_split;
-	char			*new_tmp;
 	
-	// Première passe: compter le nombre total d'arguments après expansion
-	for (i = 0; i < cmd->ac; i++)
+	i = 0;
+	new_ac = 0;
+	while (i < cmd->ac)
 	{
-		has_quote = 0;
-		will_split = 0;
-		
-		// Vérifier si l'argument contient des guillemets
-		word = cmd->list_word[i];
-		while (word)
-		{
-			if (word->type == T_S_QUOTE || word->type == T_D_QUOTE)
-				has_quote = 1;
-			
-			// Si c'est un NO_QUOTE et contient des espaces
-			if (word->type == T_NO_QUOTE && (ft_strchr(word->content, ' ')
-					|| ft_strchr(word->content, '\t')))
-				will_split = 1;
-			word = word->next;
-		}
-		if (has_quote || !will_split)
-			new_ac++; // Un seul argument
+		if (should_we_split(cmd->list_word[i]) == 0)
+			new_ac++;
 		else
-		{
-			// Compter combien d'arguments seront créés après la division
-			tmp = ft_strdup("");
-			word = cmd->list_word[i];
-			while (word)
-			{
-				new_tmp = ft_strjoin(tmp, word->content);
-				free(tmp);
-				tmp = new_tmp;
-				word = word->next;
-			}
-			split_words = ft_split(tmp, ' ');
-			j = 0;
-			while (split_words && split_words[j])
-				j++;
-			new_ac += j; // Ajouter le nombre de mots après division
-			free(tmp);
-			if (split_words)
-				free_array(split_words);
-		}
+			new_ac += count_arg_after_split(cmd->list_word[i]);
+		i++;
 	}
-	// Allouer le nouveau tableau d'arguments
 	new_args = malloc(sizeof(char *) * (new_ac + 1));
 	if (!new_args)
 		return (1);
-	// Deuxième passe: remplir le nouveau tableau d'arguments
 	k = 0;
-	for (i = 0; i < cmd->ac; i++)
-	{
-		has_quote = 0;
-		will_split = 0;
-		// Vérifier si l'argument contient des guillemets
-		word = cmd->list_word[i];
-		while (word)
-		{
-			if (word->type == T_S_QUOTE || word->type == T_D_QUOTE)
-				has_quote = 1;
-			// Si c'est un NO_QUOTE et contient des espaces
-			if (word->type == T_NO_QUOTE && (ft_strchr(word->content, ' ') || 
-											 ft_strchr(word->content, '\t')))
-				will_split = 1;
-			word = word->next;
-		}
-		if (has_quote || !will_split)
-		{
-			// Traiter comme un seul argument
-			tmp = ft_strdup("");
-			word = cmd->list_word[i];
-			while (word)
-			{
-				new_tmp = ft_strjoin(tmp, word->content);
-				free(tmp);
-				tmp = new_tmp;
-				word = word->next;
-			}
-			new_args[k++] = tmp;
-		}
-		else
-		{
-			// Diviser l'argument en plusieurs selon les espaces
-			tmp = ft_strdup("");
-			word = cmd->list_word[i];
-			while (word)
-			{
-				new_tmp = ft_strjoin(tmp, word->content);
-				free(tmp);
-				tmp = new_tmp;
-				word = word->next;
-			}
-			split_words = ft_split(tmp, ' ');
-			j = 0;
-			while (split_words && split_words[j])
-			{
-				new_args[k++] = ft_strdup(split_words[j]);
-				j++;
-			}
-			free(tmp);
-			if (split_words)
-				free_array(split_words);
-		}
-	}
-	new_args[k] = NULL;
-	// Libérer l'ancien tableau d'arguments
-	tmp_args = cmd->arg;
-	cmd->arg = new_args;
-	cmd->ac = new_ac;
-	// Mettre à jour le nom de la commande si nécessaire
-	if (cmd->ac > 0)
-	{
-		if (cmd->name)
-			free(cmd->name);
-		cmd->name = ft_strdup(cmd->arg[0]);
-	}
-	// Libérer l'ancien tableau
-	if (tmp_args)
-	{
-		for (i = 0; tmp_args[i]; i++)
-			free(tmp_args[i]);
-		free(tmp_args);
-	}
+	i = 0;
+	while (i < cmd->ac)
+		fill_new_args(cmd->list_word[i++], &k, new_args);
+	replace_args_and_free(new_args, cmd, k, new_ac);
 	return (0);
 }
+
+
 
 /*
  * Version modifiée de expand_var qui utilise la fonction rebuild_command_arg améliorée
