e8f8763c835b678cfd9b85c298ff2b1bf7d6e6be
eahmeti <eahmeti@student.42lausanne.ch>
2025-04-21 16:18:03 +0200

    main norminette

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: eahmeti <eahmeti@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 21:30:32 by eahmeti           #+#    #+#             */
-/*   Updated: 2025/04/21 16:13:01 by eahmeti          ###   ########.fr       */
+/*   Updated: 2025/04/21 16:17:57 by eahmeti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,133 +15,133 @@
 sig_atomic_t	g_sigint_received;
 
 // // Fonction pour lire une ligne depuis stdin en mode non-interactif
-// static char	*get_next_line(int fd)
-// {
-// 	char	*line;
-// 	char	buffer[10001];
-// 	int		bytes_read;
-// 	int		i;
+static char	*get_next_line(int fd)
+{
+	char	*line;
+	char	buffer[10001];
+	int		bytes_read;
+	int		i;
 
-// 	i = 0;
-// 	while ((bytes_read = read(fd, &buffer[i], 1)) > 0 && buffer[i] != '\n')
-// 		i++;
-// 	if (bytes_read <= 0 && i == 0)
-// 		return (NULL);
-// 	buffer[i] = '\0';
-// 	line = ft_strdup(buffer);
-// 	return (line);
-// }
+	i = 0;
+	while ((bytes_read = read(fd, &buffer[i], 1)) > 0 && buffer[i] != '\n')
+		i++;
+	if (bytes_read <= 0 && i == 0)
+		return (NULL);
+	buffer[i] = '\0';
+	line = ft_strdup(buffer);
+	return (line);
+}
 
-// int	main(int ac, char **av, char **envp)
-// {
-// 	t_shell		*shell;
-// 	char		*input;
-// 	t_token		*tokens;
-// 	t_ast		*ast;
-// 	int			is_interactive;
+int	main(int ac, char **av, char **envp)
+{
+	t_shell		*shell;
+	char		*input;
+	t_token		*tokens;
+	t_ast		*ast;
+	int			is_interactive;
 
-// 	(void)ac;
-// 	(void)av;
-// 	tokens = NULL;
-// 	ast = NULL;
+	(void)ac;
+	(void)av;
+	tokens = NULL;
+	ast = NULL;
 	
-// 	// Initialisation du shell et des signaux
-// 	shell = init_shell(envp);
-// 	if (!shell)
-// 		return (1);
-// 	if (setup_signals() == -1)
-// 		return (free_env_list(shell->env), free(shell), 1);
+	// Initialisation du shell et des signaux
+	shell = init_shell(envp);
+	if (!shell)
+		return (1);
+	if (setup_signals() == -1)
+		return (free_env_list(shell->env), free(shell), 1);
 	
-// 	// Vérifier si on est en mode interactif
-// 	is_interactive = isatty(fileno(stdin));
+	// Vérifier si on est en mode interactif
+	is_interactive = isatty(fileno(stdin));
 	
-// 	// Boucle principale
-// 	while (1)
-// 	{
-// 		g_sigint_received = 0;
+	// Boucle principale
+	while (1)
+	{
+		g_sigint_received = 0;
 		
-// 		// Lecture différente selon le mode
-// 		if (is_interactive)
-// 		{
-// 			input = readline("miniHell$ ");
-// 			// Gestion de EOF (Ctrl+D)
-// 			if (!input)
-// 			{
-// 				printf("exit\n");
-// 				break;
-// 			}
-// 		}
-// 		else
-// 		{
-// 			// Mode non-interactif
-// 			input = get_next_line(fileno(stdin));
-// 			if (!input)
-// 				break;
-// 		}
+		// Lecture différente selon le mode
+		if (is_interactive)
+		{
+			input = readline("miniHell$ ");
+			// Gestion de EOF (Ctrl+D)
+			if (!input)
+			{
+				printf("exit\n");
+				break;
+			}
+		}
+		else
+		{
+			// Mode non-interactif
+			input = get_next_line(fileno(stdin));
+			if (!input)
+				break;
+		}
 		
-// 		// Si l'entrée n'est pas vide
-// 		if (*input)
-// 		{
-// 			// Ajouter à l'historique seulement en mode interactif
-// 			if (is_interactive)
-// 				add_history(input);
+		// Si l'entrée n'est pas vide
+		if (*input)
+		{
+			// Ajouter à l'historique seulement en mode interactif
+			if (is_interactive)
+				add_history(input);
 			
-// 			// Nettoyer les structures précédentes
-// 			if (tokens)
-// 				free_tokens(tokens);
-// 			if (ast)
-// 				free_ast(ast);
+			// Nettoyer les structures précédentes
+			if (tokens)
+				free_tokens(tokens);
+			if (ast)
+				free_ast(ast);
 			
-// 			// Analyser l'entrée
-// 			tokens = tokenize(input);
+			// Analyser l'entrée
+			tokens = tokenize(input);
 
-// 			if (tokens)
-// 			{
-// 				// Vérifier la syntaxe des parenthèses
-// 				if (check_syntax_error_parenthesis(tokens))
-// 				{
-// 					// Parser et exécuter
-// 					ast = parse_tokens(tokens);
+			if (tokens)
+			{
+				// Vérifier la syntaxe des parenthèses
+				if (check_syntax_error_parenthesis(tokens))
+				{
+					// Parser et exécuter
+					ast = parse_tokens(tokens);
 					
-// 					// Après avoir exécuté la commande en mode non-interactif
-// 					if (ast)
-// 					{
-// 						launch_heredoc(ast, shell);
-// 						shell->exit_status = execute_ast(ast, shell);
-// 						cleanup_heredoc_files(shell->cmd);
+					// Après avoir exécuté la commande en mode non-interactif
+					if (ast)
+					{
+						launch_heredoc(ast, shell);
+						shell->exit_status = execute_ast(ast, shell);
+						cleanup_heredoc_files(shell->cmd);
 						
-// 						// Ajoute cette condition pour quitter immédiatement en mode non-interactif
-// 						if (!is_interactive)
-// 						{
-// 							// Nettoyer les ressources
-// 							if (tokens)
-// 								free_tokens(tokens);
-// 							if (ast)
-// 								free_ast(ast);
-// 							rl_clear_history();
-// 							free_env_list(shell->env);
+						// Ajoute cette condition pour quitter immédiatement en mode non-interactif
+						if (!is_interactive)
+						{
+							// Nettoyer les ressources
+							if (tokens)
+								free_tokens(tokens);
+							if (ast)
+								free_ast(ast);
+							rl_clear_history();
+							free_env_list(shell->env);
 							
-// 							// Sortir avec le code d'erreur approprié
-// 							exit(shell->exit_status);  // Important : utilise exit() pour quitter immédiatement
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-// 		free(input);
-// 	}
+							// Sortir avec le code d'erreur approprié
+							exit(shell->exit_status);  // Important : utilise exit() pour quitter immédiatement
+						}
+					}
+				}
+			}
+		}
+		free(input);
+	}
 	
-// 	// Nettoyage avant de quitter
-// 	if (tokens)
-// 		free_tokens(tokens);
-// 	if (ast)
-// 		free_ast(ast);
-// 	rl_clear_history();
-// 	free_env_list(shell->env);
-// 	free(shell);
+	// Nettoyage avant de quitter
+	if (tokens)
+		free_tokens(tokens);
+	if (ast)
+		free_ast(ast);
+	rl_clear_history();
+	free_env_list(shell->env);
+	free(shell);
 	
-// 	return (shell->exit_status);
-// }
+	return (shell->exit_status);
+}
 
 void free_shell(t_ast *ast, t_token *tokens, t_shell *shell)
 {
